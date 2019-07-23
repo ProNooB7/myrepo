ed54582428f0b3469fa91cd2430731e1e90f3eca
pradeep kumar <pradeepkumar13307@gmail.com>
2019-07-23 14:37:33 +0530

    i am adding maven project

---
+++ src/main/java/maven_project/com/app/amdocs/company/App.java
@@ -0,0 +1,13 @@
+package maven_project.com.app.amdocs.company;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
